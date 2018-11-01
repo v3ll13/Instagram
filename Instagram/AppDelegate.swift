b8d4c24c8d1180cc104d1866f2299cd073ddad28
Vellie <laguerre.vellie@gmail.com>
2018-11-01 12:50:47 -0400

    parse configured

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+            configuration.applicationId = "Instagram"
+            configuration.clientKey = "asdfghjklzxcvbnmpoiuytrewq"
+            configuration.server = "https://still-falls-72574.heroku.com/parse"
+        }))
+        
         return true
     }
 
